dec313a8c3f1f7b76a8340bf3144e8b5e8136d82
Artur Comoli <artur.comoli@gmail.com>
2021-12-27 17:39:41 -0300

    versao 1 desktop

--- assets/js/noticias.js
+++ assets/js/noticias.js
@@ -13,19 +13,76 @@ const imgNoticia = document.querySelector(".carrossel--noticias>figure>img")
 function noticias() {
     let contador = 0;
 
-    aumentar.addEventListener("click", () => {        
-        if (contador < arrNoticias.length - 1) {                     
-            contador++;                 
+    aumentar.addEventListener("click", () => {
+        if (contador < arrNoticias.length - 1) {
+            contador++;
         } else {
             contador = 0;
-        }   
-        imgNoticia.src = arrNoticias[contador]     
+        }
+        imgNoticia.src = arrNoticias[contador]
     })
     diminuir.addEventListener("click", () => {
         if (contador > 0) {
-            contador--;            
+            contador--;
         }
         imgNoticia.src = arrNoticias[contador];
     })
 }
 noticias()
+
+// Noticias desktop
+const objetoNoticias = [
+    {
+        titulo: "Título da notícia 1",
+        texto: "Texto da notícia 1",
+        imgSrc: "./assets/img/noticias/F22-Raptor.jpg",
+    },
+    {
+        titulo: "Título da notícia 2",
+        texto: "Texto da notícia 2",
+        imgSrc: "./assets/img/noticias/F22-Raptor.jpg",
+    },
+    {
+        titulo: "Título da notícia 3",
+        texto: "Texto da notícia 3",
+        imgSrc: "./assets/img/noticias/F22-Raptor.jpg",
+    },
+    {
+        titulo: "Título da notícia 4",
+        texto: "Texto da notícia 4",
+        imgSrc: "./assets/img/noticias/F22-Raptor.jpg",
+    },
+    {
+        titulo: "Título da notícia 5",
+        texto: "Texto da notícia 5",
+        imgSrc: "./assets/img/noticias/F22-Raptor.jpg",
+    },
+]
+
+
+
+const noticiasDesktop = document.querySelector(".noticias--desktop");
+function criarNoticias(objeto) {    
+    for (let i = 0; i < objeto.length; i++) {
+        const {titulo, texto, imgSrc} = objeto[i];        
+        const div = document.createElement("div");
+        div.classList.add("quadro--noticia");
+        const figure = document.createElement("figure");
+        const img = document.createElement("img");
+        img.src = imgSrc;
+        const figcaption = document.createElement("figcaption");
+        figcaption.textContent = titulo;
+        const h3 = document.createElement("h3");
+        h3.textContent = titulo;
+        const p = document.createElement("p");
+        p.textContent = texto;
+
+        figure.appendChild(img);
+        figure.appendChild(figcaption);
+        div.appendChild(figure);
+        div.appendChild(h3);
+        div.appendChild(p);
+        noticiasDesktop.appendChild(div)
+    }
+}
+criarNoticias(objetoNoticias);
