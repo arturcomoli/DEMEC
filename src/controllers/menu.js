bdd01fa52373d7c4448be7d36dbd6547beb6c424
Artur Comoli <artur.comoli@gmail.com>
2022-02-13 13:48:26 -0300

    feat(menu): menu added

---
+++ src/controllers/menu.js
@@ -0,0 +1,5 @@
+$("#demoMenu").aceResponsiveMenu({
+  resizeWidth: "768", // Set the breakpoint same in Media query
+  animationSpeed: "fast", //slow, medium, fast
+  accoridonExpAll: false, //Expands all the accordion menu on click
+});
