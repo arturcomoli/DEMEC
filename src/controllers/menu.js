b396b28abe334f062bbd72f9c85f2b7d29ff8196
Artur Comoli <artur.comoli@gmail.com>
2022-02-13 16:47:11 -0300

    fix(style): menu fixxed

--- src/controllers/menu.js
+++ src/controllers/menu.js
@@ -1,5 +1,5 @@
 $("#demoMenu").aceResponsiveMenu({
-  resizeWidth: "768", // Set the breakpoint same in Media query
-  animationSpeed: "fast", //slow, medium, fast
-  accoridonExpAll: false, //Expands all the accordion menu on click
+  resizeWidth: "900", // Set the breakpoint same in Media query
+  animationSpeed: "medium", //slow, medium, fast
+  accoridonExpAll: true, //Expands all the accordion menu on click
 });
